bafc0f5e02b12a2f4a27408eb91c253416fceb02
root <root@asusjayson.home>
2024-09-10 14:58:50 +0200

    maj

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,12 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from "../Pages/Login.jsx";
 import Register from "../Pages/Register.jsx";
-<<<<<<< HEAD
-import Messaging from "../Pages/Messaging.jsx";
-import Message from "../Pages/Message.jsx";
-=======
 import { Accueil } from "../Pages/Accueil.jsx";
->>>>>>> main
 
 export const AppRoutes = () => {
     return (
@@ -14,14 +9,8 @@ export const AppRoutes = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-                <Route path="/messaging" element={<Messaging />} />
-                <Route path="/message" element={<Message />} />
-                <Route path="*" element={<h1>404 Not Found</h1>} />
-=======
                 <Route path="/Accueil" element={<Accueil />} />
-                    <Route path="*" element={<h1>404 Not Found</h1>} />
->>>>>>> main
+                <Route path="*" element={<h1>404 Not Found</h1>} />
             </Routes>
         </BrowserRouter>
     );
