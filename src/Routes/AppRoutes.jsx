68758c112f46d24bbe70108e6db0ca9dd53a60bd
Mr_Json <92954227+mester-Json@users.noreply.github.com>
2024-09-10 15:02:53 +0200

    Update AppRoutes.jsx

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from "../Pages/Login.jsx";
 import Register from "../Pages/Register.jsx";
+
+import Messaging from "../Pages/Messaging.jsx";
+import Message from "../Pages/Message.jsx";
 import { Accueil } from "../Pages/Accueil.jsx";
 
 export const AppRoutes = () => {
@@ -9,10 +12,12 @@ export const AppRoutes = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/Accueil" element={<Accueil />} />
+                <Route path="/messaging" element={<Messaging />} />
+                <Route path="/message" element={<Message />} />
                 <Route path="*" element={<h1>404 Not Found</h1>} />
+                <Route path="/Accueil" element={<Accueil />} />
+                    <Route path="*" element={<h1>404 Not Found</h1>} />
             </Routes>
         </BrowserRouter>
     );
 }
-
