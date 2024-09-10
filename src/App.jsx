1d0ea5ec0066f3816e47653bc59dfaef3780111f
root <root@asusjayson.home>
2024-09-10 14:30:40 +0200

    Ajout des Modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import AppRoutes from './Routes/AppRoutes.jsx';
+import { AppRoutes } from './Routes/AppRoutes.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
