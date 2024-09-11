90399eb08be0d364f25c39255d114703f4315ea4
root <root@asusjayson.home>
2024-09-11 14:42:04 +0200

    MAj

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -1,10 +1,10 @@
-import {Nav } from "../Components/Nav/Nav.jsx";
-import { Footer }from "../Components/Footer/Footer.jsx";
+import { Nav } from "../Components/Nav/Nav.jsx";
+import { Footer } from "../Components/Footer/Footer.jsx";
 import { ChatApp } from "../Components/Messaging/ChatApp.jsx";
 
 //Conversation 
 
-export const  Message = () => {
+export const Message = () => {
     return (
         <>
             <Nav />
