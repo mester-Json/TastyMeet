79d367f9ae49a7e2fa3e6709a230c24f7831b74f
Mr_Json <92954227+mester-Json@users.noreply.github.com>
2024-09-11 14:01:18 +0200

    Update FormRegister.jsx

--- src/Components/FormRegister/FormRegister.jsx
+++ src/Components/FormRegister/FormRegister.jsx
@@ -1,26 +1,30 @@
 /*---------------------------- import ------------------------------------------------*/
-import { useState, useEffect } from "react";
+import { useState , useEffect } from "react";
 import {
-  Form1,
-  Form2,
-  InputField,
-  InputAvatar,
-  Button,
-  Select,
-  Description,
-  P,
-  ImgAvatar,
-  ImgEye,
-  Div,
-  Div2,
+    FormularRegister ,
+    FormMoreInfo ,
+    InputField ,
+    InputAvatar ,
+    Button ,
+    Select ,
+    Description ,
+    P ,
+    LabelError ,
+    TitleForm1 ,
+    TitleForm2 ,
+    ImgAvatar ,
+    ImgEye ,
+    Div ,
+    Div2 ,
+    DivError ,
 } from "./FormRegister.style.jsx";
 import avatarDefault from "../../Resources/Images/avatar.png";
 import eyeOpen from "../../Resources/Images/eye.svg";
 import eyeClose from "../../Resources/Images/eye-slash.svg";
 
 /*----------------------------------------------------------------------------*/
-export const FormRegister = () => {
 
+function FormRegister () {
     /*---------------------------- useState ------------------------------------------------*/
     const [ errors , setErrors ] = useState ( {
         firstName: "" ,
@@ -33,13 +37,14 @@ export const FormRegister = () => {
     const [ email , setEmail ] = useState ( "" );
     const [ password , setPassword ] = useState ( "" );
     const [ showPassword , setShowPassword ] = useState ( false );
+    const [ showError , setShowError ] = useState ( false );
     const [ firstName , setFirstName ] = useState ( "" );
     const [ name , setName ] = useState ( "" );
     const [ gender , setGender ] = useState ( "" );
     const [ birthDate , setBirthDate ] = useState ( "" );
     const [ isAdult , setIsAdult ] = useState ( null );
     const [ isLoading , setIsLoading ] = useState ( false );
-    const [ currentForm , setCurrentForm ] = useState ( "form1" );
+    const [ currentForm , setCurrentForm ] = useState ( "formularRegister" );
     const [ description , setDescription ] = useState ( "" );
     const [ phone , setPhone ] = useState ( null );
     const [ avatar , setAvatar ] = useState ( avatarDefault );
@@ -62,46 +67,129 @@ export const FormRegister = () => {
     /*---------------------------- handle... ------------------------------------------------*/
     // Fonction d'inscription
     const handleLogin = async () => {
-        if (validateForm ()) {
-            try {
-                const response = await fetch ( "http://localhost:5173/register" , {
-                    method: "POST" ,
-                    headers: {"Content-Type": "application/json"} ,
-                    body: JSON.stringify ( {
-                        firstName ,
-                        name ,
-                        gender ,
-                        email ,
-                        password ,
-                        birthDate ,
-                    } ) ,
-                } );
+        const newErrors = {};
 
-                if (! response.ok) {
-                    throw new Error ( "Erreur lors de l'inscription" );
+        switch (currentForm) {
+            case "formularRegister":
+                if (! firstName) {
+                    newErrors.firstName = "Le nom est requis";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
                 }
+                if (! name) {
+                    newErrors.name = "Le prénom est requis";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (! email) {
+                    newErrors.email = "L'email est requis";
+                    setShowError ( true );
+                } else if (! /^[^\s@]+@[^\s@]+.[^\s@]+$/.test ( email )) {
+                    newErrors.email = "L'email n'est pas valide";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (! password) {
+                    newErrors.password = "Le mot de passe est requis";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (gender === "") {
+                    newErrors.gender = "Veuillez choisir un genre";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (! birthDate) {
+                    newErrors.birthDate = "La date de naissance est requise";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (isAdult === false) {
+                    newErrors.birthDate = "Vous devez être majeur pour vous inscrire";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                break;
+            case "formMoreInfo":
+                if (avatar === avatarDefault) {
+                    newErrors.avatar = "L'avatar est requis";
+                    setShowError ( true );
+                } else {
+                    setShowError ( false );
+                }
+                if (phone === null) {
+                    newErrors.phone = "Le numéro de téléphone est requis";
+                    setShowError ( true );
+                } else if (! /^\+?[0-9]{10,15}$/.test ( phone )) {
+                    newErrors.phone = "Le numéro de téléphone n'est pas valide";
+                } else {
+                    setShowError ( false );
+                }
+                break;
+            default:
+                console.error ( "Formulaire non reconnu" );
+                break;
+        }
 
-                const data = await response.json ();
-                console.log ( "Inscription réussie:" , data );
-                //handleFormSwitch();//Change de formulaire
-            } catch (error) {
-                console.error ( "Erreur:" , error );
-            }
+        // Si pas d'erreurs, soumettre le formulaire
+        if (Object.keys ( newErrors ).length === 0) {
+            // Si pas d'erreurs, effectuer la requête POST pour soumettre les données
+            setIsLoading ( true );
+            fetch ( "http://localhost:5173/register" , {
+                method: "POST" ,
+                headers: {"Content-Type": "application/json"} ,
+                body: JSON.stringify ( {
+                    firstName ,
+                    name ,
+                    gender ,
+                    email ,
+                    password ,
+                    birthDate ,
+                    phone ,
+                    description ,
+                    avatar ,
+                } ) ,
+            } )
+                .then ( (response) => {
+                    if (! response.ok) {
+                        throw new Error ( "Erreur lors de l'inscription" );
+                    }
+                    return response.json ();
+                } )
+                .then ( (data) => {
+                    console.log ( "Inscription réussie:" , data );
+                    // Action après inscription réussie
+                } )
+                .catch ( (error) => {
+                    console.error ( "Erreur:" , error );
+                } )
+                .finally ( () => {
+                    setIsLoading ( false );
+                } );
+        } else {
+            setErrors ( newErrors );
         }
     };
 
-    // set la date de naissance
+// set la date de naissance
     const handleDateChange = (event) => {
         setBirthDate ( event.target.value );
     };
 
-    // Fonction test pour afficher le form2
+// Fonction test pour afficher le form2
     const handleNext = async () => {
         handleLogin ();
-        handleFormSwitch ();
+        handleFormSwitch ( "formMoreInfo" );
     };
 
-    // Changement de l'avatar par défaut, par le nouvelle avatar
+// Changement de l'avatar par défaut, par le nouvelle avatar
     const handlePictureChange = (e) => {
         const file = e.target.files[0]; // Sélectionne le premier fichier
         if (file) {
@@ -113,7 +201,7 @@ export const FormRegister = () => {
         }
     };
 
-    // Affiche ou non me mot de passe et changer l'oeil
+// Affiche ou non me mot de passe et changer l'oeil
     const handleShowPassword = () => {
         if (showPassword === false) {
             setShowPassword ( ! showPassword );
@@ -124,47 +212,30 @@ export const FormRegister = () => {
         }
     };
 
-    // Ajuster dynamiquement la taille du textarea selon le contenu
+// Ajuster dynamiquement la taille du textarea selon le contenu
     const handleAutoRedimText = (e) => {
         e.target.style.height = "auto";
         e.target.style.height = e.target.scrollHeight + "px";
     };
 
-    // Changement de formulaire sur la même page
+// Changement de formulaire sur la même page
     const handleFormSwitch = (formName) => {
         setCurrentForm ( formName );
-    };
-    /*----------------------------------------------------------------------------*/
-    /*---------------------------- ValidateForm ------------------------------------------------*/
-    // Verifi le contenu du formulaire
-    const validateForm = () => {
-        const newErrors = {};
-
-        if (! firstName) newErrors.firstName = "Le nom est requis";
-        if (! name) newErrors.name = "Le prénom est requis";
-        if (! email) {
-            newErrors.email = "L'email est requis";
-        } else if (! /\S+@\S+\.\S+/.test ( email )) {
-            // Utiliser une expression régulière pour vérifier le format de l'email
-            newErrors.email = "L'email n'est pas valide";
-        }
-        if (! password) newErrors.password = "Le mot de passe est requis";
-        if (gender === "") newErrors.gender = "Veuillez choisir un genre";
-        if (! birthDate) newErrors.birthDate = "La date de naissance est requise";
-        if (isAdult === false)
-            newErrors.birthDate = "Vous devez être majeur pour vous inscrire";
-
-        setErrors ( newErrors );
-        return Object.keys ( newErrors ).length === 0;
     };
     /*----------------------------------------------------------------------------*/
 
     return (
         <>
-            { currentForm === "form1" && (
-                <Form1>
+            { currentForm === "formularRegister" && (
+                <FormularRegister>
+                    <TitleForm1>Inscription</TitleForm1>
                     <Div>
                         {/*---------------------------- FIRSTNAME ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.firstName }
+                            </LabelError>
+                        </DivError>
                         <InputField
                             type="text"
                             placeholder="Nom"
@@ -172,11 +243,13 @@ export const FormRegister = () => {
                             value={ firstName }
                             onChange={ (e) => setFirstName ( e.target.value ) }
                         />
-                        { errors.firstName && (
-                            <p style={ {color: "red"} }>{ errors.firstName }</p>
-                        ) }
                         {/* ----------------------------------------------------------------------------------- */ }
                         {/*---------------------------- GENDER ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.gender }
+                            </LabelError>
+                        </DivError>
                         <Select
                             id="options"
                             value={ gender }
@@ -188,20 +261,28 @@ export const FormRegister = () => {
                             <option value="Furry">Furry</option>
                             <option value="NonBinaire">Non Binaire</option>
                         </Select>
-                        { errors.gender && <p style={ {color: "red"} }>{ errors.gender }</p> }
                         {/* ----------------------------------------------------------------------------------- */ }
                         {/*---------------------------- EMAIL ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.email }
+                            </LabelError>
+                        </DivError>
                         <InputField
-                            type="text"
+                            type="email"
                             placeholder="Adresse e-mail"
                             value={ email }
                             onChange={ (e) => setEmail ( e.target.value ) }
                         />
-                        { errors.email && <p style={ {color: "red"} }>{ errors.email }</p> }
                         {/* ----------------------------------------------------------------------------------- */ }
                         {/*---------------------------- NAME ------------------------------------------------*/ }
                     </Div>
                     <Div2>
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.name }
+                            </LabelError>
+                        </DivError>
                         <InputField
                             type="text"
                             placeholder="Prénom"
@@ -209,19 +290,25 @@ export const FormRegister = () => {
                             value={ name }
                             onChange={ (e) => setName ( e.target.value ) }
                         />
-                        { errors.name && <p style={ {color: "red"} }>{ errors.name }</p> }
                         {/* ----------------------------------------------------------------------------------- */ }
                         {/*---------------------------- BIRTHDATE ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.birthDate }
+                            </LabelError>
+                        </DivError>
                         <InputField
                             type="date"
                             value={ birthDate }
                             onChange={ handleDateChange }
                         />
-                        { errors.birthDate && (
-                            <p style={ {color: "red"} }>{ errors.birthDate }</p>
-                        ) }
                         {/* ----------------------------------------------------------------------------------- */ }
                         {/*---------------------------- PASSWORD ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.password }
+                            </LabelError>
+                        </DivError>
                         <InputField
                             id="pwd"
                             type={ showPassword ? "text" : "password" }
@@ -231,20 +318,24 @@ export const FormRegister = () => {
                             onChange={ (e) => setPassword ( e.target.value ) }
                         />
                         <ImgEye src={ eye } onClick={ handleShowPassword }/>
-                        { errors.password && (
-                            <p style={ {color: "red"} }>{ errors.password }</p>
-                        ) }
                         {/* ----------------------------------------------------------------------------------- */ }
                     </Div2>
                     <Button type="button" onClick={ handleLogin } disabled={ isLoading }>
                         { isLoading ? "Validation..." : "Suivant" }
                     </Button>
-                </Form1>
+                </FormularRegister>
             ) }
 
-            { currentForm === "form2" && (
-                <Form2>
+            { currentForm === "formMoreInfo" && (
+                <FormMoreInfo>
+                    <TitleForm2>Informations supplémentaires</TitleForm2>
                     <Div>
+                        {/*---------------------------- AVATAR ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.avatar }
+                            </LabelError>
+                        </DivError>
                         <ImgAvatar src={ avatar }/>
                         <InputAvatar
                             type="file" // Input de type file
@@ -252,8 +343,15 @@ export const FormRegister = () => {
                             accept=".jpg, .jpeg, .png" // extension avatar accepter
                             onChange={ handlePictureChange }
                         />
+                        {/* ----------------------------------------------------------------------------------- */ }
                     </Div>
                     <Div2>
+                        {/*---------------------------- PHONE ------------------------------------------------*/ }
+                        <DivError visibility={ showError ? 'visible' : 'hidden' }>
+                            <LabelError>
+                                { errors.phone }
+                            </LabelError>
+                        </DivError>
                         <InputField
                             type="tel"
                             placeholder="Tel"
@@ -262,6 +360,8 @@ export const FormRegister = () => {
                             value={ phone }
                             onChange={ (e) => setPhone ( e.target.value ) }
                         />
+                        {/* ----------------------------------------------------------------------------------- */ }
+                        {/*---------------------------- DESCRIPTION ------------------------------------------------*/ }
                         <Description
                             name="description"
                             onChange={ (e) => setDescription ( e.target.value ) }
@@ -274,8 +374,13 @@ export const FormRegister = () => {
                         />
                         <P>{ description.length }/200 caractères utilisés</P>
                     </Div2>
-                </Form2>
+                    <Button type="button" onClick={ handleLogin } disabled={ isLoading }>
+                        { isLoading ? "Inscription...." : "S'inscrire" }
+                    </Button>
+                </FormMoreInfo>
             ) }
         </>
     );
 }
+
+export default FormRegister;
