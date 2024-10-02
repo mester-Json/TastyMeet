4a572a4a2a6a0eaac3e60e389af7352902f94888
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-10-02 14:04:34 +0200

    Responsive page Help

--- src/Components/FormHelp/FormHelp.style.jsx
+++ src/Components/FormHelp/FormHelp.style.jsx
@@ -1,71 +1,53 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1000px;
-  padding: 20px;
+  width: 60%;
+  padding: 1vw;
   background-color: #7c4a42;
   color: #ffffff;
-  border-radius: 10px;
+  border-radius: 0.5vw;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 `;
 
 export const Header = styled.header`
   text-align: center;
-  margin-bottom: 20px;
 `;
 
 export const Title = styled.h1`
   margin: 0;
-  font-size: 24px;
+  font-size: 1.5vw;
   text-transform: uppercase;
   text-align: start;
 `;
 
 export const HelpText = styled.p`
   background-color: #8b5a50;
-  padding: 15px;
-  border-radius: 10px;
+  padding: 0.8vw;
+  border-radius: 0.5vw;
   text-align: start;
+  font-size: 0.9vw;
 `;
 
 export const QuestionContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  font-size: 0.8vw;
 `;
 
 export const QuestionButton = styled.button`
   background-color: #9c6a5f;
   color: #ffffff;
-  padding: 15px;
+  padding: 0.8vw;
   border: none;
-  border-radius: 10px;
+  border-radius: 0.5vw;
   cursor: pointer;
-  font-size: 18px;
+  font-size: 0.9vw;
   text-align: left;
-  margin-bottom: 10px;
 
   &:hover {
     background-color: #a7796f;
   }
 `;
-
-// export const QuestionContent = styled.div`
-//   max-height: ${({ isOpen }) =>
-//     isOpen ? "200px" : "0"}; /* Limite la hauteur du contenu */
-//   overflow: hidden;
-//   transition: max-height 0.3s ease-out; /* Transition pour le déroulement */
-//   padding: ${({ isOpen }) =>
-//     isOpen ? "10px" : "0"}; /* Ajout de padding uniquement si ouvert */
-//   background-color: #9c6a5f;
-//   border-radius: 5px;
-//   margin-bottom: 10px;
-//   color: #ffffff;
-
-//   p {
-//     margin: 0;
-//   }
-// `;
 
 export const QuestionContent = styled.div.attrs(({ opened }) => ({
   "aria-expanded": opened,
@@ -75,10 +57,11 @@ export const QuestionContent = styled.div.attrs(({ opened }) => ({
   overflow: hidden;
   transition: max-height 0.3s ease-out; /* Transition pour le déroulement */
   padding: ${({ opened }) =>
-    opened === "true"? "10px" : "0"}; /* Ajout de padding uniquement si ouvert */
+    opened === "true"? "0.8vw;" : "0"}; /* Ajout de padding uniquement si ouvert */
   background-color: #9c6a5f;
-  border-radius: 5px;
-  margin-bottom: 10px;
+  border-radius: 0.25vw;
+  margin-top: 0.25vw;
+  margin-bottom: 0.25vw;
   color: #ffffff;
   opacity: ${({ opened }) =>
     opened === "true"? "1" : "0"}; /* Transition de l'opacité */
